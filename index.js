666eaf1f288cfb6d48f2b562e9a73f7bba721086
Michael Shumaker <74860626+16pxMs@users.noreply.github.com>
2024-09-30 15:06:28 +0300

    Assignnment completed
    User should be able to add scores using all buttonns for both home and guest

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-let count = 0
-let homeScore = document.getElementById(homeScore)
+let count = 0;
+let homeScore = document.getElementById('homeScore')
 
 function addOnePoint() {
-    let result = count + 1
-    homeScore.textContent = result 
+    count += 1
+    homeScore.textContent = count 
 }
 
+function addTwoPoint() {
+    count += 2
+    homeScore.textContent = count 
+}
+
+function addThreePoint() {
+    count += 3
+    homeScore.textContent = count
+}
+
+let score = 0
+let guestScore = document.getElementById('guestScore')
+
+function addPointOne() {
+    score += 1
+    guestScore.textContent = score
+} 
+
+function addPointTwo() {
+    score += 2
+    guestScore.textContent = score
+
+}
+
+function addPointThree() {
+    score += 3
+    guestScore.textContent = score
+}
